c674ae6fe29163308173495a6947b1158c559cac
Robert Leidl <robert.leidl@namadgi.com>
2019-09-12 18:13:44 +1000

    Added Download Functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -39,7 +39,7 @@ export class AppComponent implements OnInit {
     };
   }
 
-  onSubmit(token) {
+  onSubmitUpload(token) {
     const formData = new FormData();
     formData.append('file', this.fileData);
     const  headers = new  HttpHeaders().set('Authorization', token);
@@ -50,8 +50,29 @@ export class AppComponent implements OnInit {
 
     this.http.post <Doco>('https://devdoc.gravityfusion.com.au/upload', formData, {headers})
       .subscribe(resp => {
-          this.uploadedFilePath =  resp.filename;
-          alert('SUCCESS !!');
+        this.uploadedFilePath =  resp.filename;
+        alert('SUCCESS !!');
+      });
+  }
+
+  // downloadFile(): Observable<HttpResponse<Blob>>{
+  // 		return this.http.get('http://localhost:8080/employees/download', { responseType: ResponseContentType.Blob });
+  //    }
+
+  onSubmitDownload(token, filename) {
+    const  headers = new  HttpHeaders().set('Authorization', token);
+    this.http.get (`https://devdoc.gravityfusion.com.au/download/${filename}`,  { headers, responseType: 'blob' })
+      .subscribe((response: any) => {
+        const dataType = response.type;
+        const binaryData = [];
+        binaryData.push(response);
+        const downloadLink = document.createElement('a');
+        downloadLink.href = window.URL.createObjectURL(new Blob(binaryData, {type: dataType}));
+        if (filename) {
+          downloadLink.setAttribute('download', filename);
+        }
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
       });
   }
 }
